c797b83488f057b4c4908b1fa997697b3ec5da56
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-05 23:02:21 -0300

    Criado Main da central

--- src/central/Main.java
+++ src/central/Main.java
@@ -1,13 +1,29 @@
 package central;
 
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setSecurityManager(null);
+		System.out.println("Ativando servidor");
+		try {
+			LocateRegistry.createRegistry(1099);
+		} catch (Exception e) {
+			System.out.println("Registro RMI ja estava ativo");
+		}
+		try {
+			ICentralRemote servidor = new Central();
+			Naming.rebind("centralaifone", servidor);
+			System.out.println("Servidor ativo");
+		} catch (Exception e2) {
+			System.out.println("Impossivel ativar servidor. Excecao: ");
+			e2.printStackTrace();
+		}
 	}
 
 }
