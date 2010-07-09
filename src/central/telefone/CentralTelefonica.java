d7b790770bba9aef3c03c9d99a4b6dd34021d31f
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-09 13:12:02 -0300

    Criado stub CentralTelefonica

---
+++ src/central/telefone/CentralTelefonica.java
@@ -0,0 +1,38 @@
+package central.telefone;
+
+import entidades.Mensagem;
+import entidades.Telefone;
+
+public class CentralTelefonica implements ICentralTelefonica {
+
+	@Override
+	public void efetuarChamada(Telefone origem, Telefone destino) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void informarChamadaEncerrada(Telefone telefone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void informarChamadaRejeitada(Telefone telefone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void confirmarAtendimento(Telefone telefone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviarMensagem(Telefone origem, Mensagem mensagem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
