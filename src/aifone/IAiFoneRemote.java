7a91d3ae4c46e7153874d8f1bebf87f7b8fd4342
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-09 01:01:13 -0300

    AiFone servidor RMI funcionando

--- src/aifone/IAiFoneRemote.java
+++ src/aifone/IAiFoneRemote.java
@@ -1,27 +1,30 @@
 package aifone;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 import entidades.Mensagem;
 import entidades.Telefone;
 
-public interface IAiFoneRemote {
+public interface IAiFoneRemote extends Remote {
 
 	/**
 	 * Recebe uma chamada telefonica
 	 * 
 	 * @param origem Telefone de origem da chamada
 	 */
-	public void receberChamada(Telefone origem);
+	public void receberChamada(Telefone origem) throws RemoteException;
 	
 	/**
 	 * Encerra uma chamada em andamento
 	 */
-	public void encerrarChamada();
+	public void encerrarChamada() throws RemoteException;
 	
 	/**
 	 * Recebe uma mensagem
 	 * 
 	 * @param mensagem Mensagem enviada
 	 */
-	public void receberMensagem(Mensagem mensagem);
+	public void receberMensagem(Mensagem mensagem) throws RemoteException;
 	
 }
--- src/aifone/Main.java
+++ src/aifone/Main.java
@@ -1,13 +1,36 @@
 package aifone;
 
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+import central.Central;
+import central.ICentralRemote;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		AiFone aifone = new AiFone();
-		aifone.testeConectarTelefone();
+		
+		System.setSecurityManager(null);
+		System.out.println("Ativando aifone");
+		try {
+			LocateRegistry.createRegistry(1100);
+		} catch (Exception e) {
+			System.out.println("Registro RMI ja estava ativo");
+		}
+		try {
+			IAiFoneRemote servidor = new AiFone();
+			Naming.rebind("aifone", servidor);
+			System.out.println("Servidor ativo");
+			((AiFone)servidor).testeConectarTelefone();
+		} catch (Exception e2) {
+			System.out.println("Impossivel ativar servidor. Excecao: ");
+			e2.printStackTrace();
+			System.exit(1);
+		}
+
 	}
 
 }
