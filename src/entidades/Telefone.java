b462014f5a04313ab8815fb4dce2e7e666a1e8ff
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-08 22:09:26 -0300

    Telefone: Gerado equals e hashcode

--- src/entidades/Telefone.java
+++ src/entidades/Telefone.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 
 public class Telefone implements Serializable {
 
-	private String numero;
-	
+	private final String numero;
+
 	public Telefone(String numero) {
 		super();
 		this.numero = numero;
@@ -15,8 +15,29 @@ public class Telefone implements Serializable {
 		return numero;
 	}
 
-	protected void setNumero(String numero) {
-		this.numero = numero;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefone other = (Telefone) obj;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		return true;
 	}
 
 	private static final long serialVersionUID = -8780538450738945360L;
