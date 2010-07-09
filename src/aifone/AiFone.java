f78eac339207ca697634579ac08a0308de871bd5
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-08 21:00:13 -0300

    M?todo refatorado e criando somente uma inst?ncia do objeto RMI.

--- src/aifone/AiFone.java
+++ src/aifone/AiFone.java
@@ -9,20 +9,35 @@ public class AiFone {
 
 	public static final String ENDERECO_CENTRAL = "rmi://alexandre11.redecasd.ita.br/centralaifone";
 
+	private ICentralRemote servidor;
+
 	public void testeConectarTelefone() throws RemoteException {
 		getInstanciaServidor().conectarTelefone(null);
 	}
 
+	/**
+	 * Obtém uma instância do objeto RMI no servidor
+	 * 
+	 * @return Objeto remoto
+	 */
 	private ICentralRemote getInstanciaServidor() {
-		ICentralRemote servidor = null;
+		if (servidor == null) {
+			criarInstanciaServidor();
+		}
+		return this.servidor;
+	}
+
+	/**
+	 * Cria a instância do objeto RMI do servidor
+	 */
+	private void criarInstanciaServidor() {
 		try {
 			servidor = (ICentralRemote) Naming.lookup(ENDERECO_CENTRAL);
-
 		} catch (Exception e) {
 			System.out
 					.println("Nao foi possivel conectar ao servidor. Excecao: ");
 			e.printStackTrace();
+			System.exit(1);
 		}
-		return servidor;
 	}
 }
