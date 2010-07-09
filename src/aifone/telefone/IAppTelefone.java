9d046de160988b78ef596dbbebfc14392ee243ee
Alexandre Martani <amartani AT gmail DOT com@localhost>
2010-07-09 13:46:32 -0300

    Criado stub apptelefone e hitwitter.

---
+++ src/aifone/telefone/IAppTelefone.java
@@ -0,0 +1,29 @@
+package aifone.telefone;
+
+import java.rmi.RemoteException;
+
+import entidades.Mensagem;
+import entidades.Telefone;
+
+public interface IAppTelefone {
+	/**
+	 * Recebe uma chamada telefonica
+	 * 
+	 * @param origem
+	 *            Telefone de origem da chamada
+	 */
+	public void receberChamada(Telefone origem);
+
+	/**
+	 * Encerra uma chamada em andamento
+	 */
+	public void encerrarChamada();
+
+	/**
+	 * Recebe uma mensagem
+	 * 
+	 * @param mensagem
+	 *            Mensagem enviada
+	 */
+	public void receberMensagem(Mensagem mensagem);
+}
