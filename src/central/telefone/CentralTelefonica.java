3b7800295528381db75573f6d2be60c6b9239d87
Roger <Roger@localhost>
2010-07-10 03:32:09 -0300

    Fa??ade da VF 2.1

--- src/central/telefone/CentralTelefonica.java
+++ src/central/telefone/CentralTelefonica.java
@@ -1,37 +1,45 @@
 package central.telefone;
 
+import central.Central;
+import central.telefone.rn.GerenciadorChamadas;
 import entidades.Mensagem;
 import entidades.Telefone;
 
 public class CentralTelefonica implements ICentralTelefonica {
+	
+	private GerenciadorChamadas gerenciadorChamadas;	
+
+	public CentralTelefonica(Central central) {
+		this.gerenciadorChamadas = new GerenciadorChamadas(central);
+	}
 
 	@Override
 	public void efetuarChamada(Telefone origem, Telefone destino) {
-		// TODO Auto-generated method stub
+		gerenciadorChamadas.efetuarChamada(origem, destino);
 		
 	}
 
 	@Override
 	public void informarChamadaEncerrada(Telefone telefone) {
-		// TODO Auto-generated method stub
+		gerenciadorChamadas.informarChamadaEncerrada(telefone);
 		
 	}
 
 	@Override
 	public void informarChamadaRejeitada(Telefone telefone) {
-		// TODO Auto-generated method stub
+		gerenciadorChamadas.informarChamadaRejeitada(telefone);
 		
 	}
 
 	@Override
 	public void confirmarAtendimento(Telefone telefone) {
-		// TODO Auto-generated method stub
+		gerenciadorChamadas.confirmarAtendimento(telefone);
 		
 	}
 
 	@Override
 	public void enviarMensagem(Telefone origem, Mensagem mensagem) {
-		// TODO Auto-generated method stub
+		gerenciadorChamadas.enviarMensagem(origem, mensagem);
 		
 	}
 
