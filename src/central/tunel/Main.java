68360cc314864d7bb9789f7855707fd30ee6a3d9
dalthon <dalthon@localhost>
2010-07-12 17:03:00 -0300

    Sei lah oq tem aki

--- src/central/tunel/Main.java
+++ src/central/tunel/Main.java
@@ -11,10 +11,11 @@ public class Main {
 	public static void main(String[] args) {
 		Tunel tunel = new Tunel();
 		// Ler tweets
-		Requisicao requisicao = new Requisicao("dalthon", "senha!", "GET", "/statuses/friends_timeline.xml");
+		Requisicao requisicao = new Requisicao("dalthon", "TWITTER3fqfgr", "GET", "/statuses/friends_timeline.xml");
+		tunel.enviarRequisicao(new Telefone("99997777"), requisicao);
 		// Twittar!
-		requisicao = new Requisicao("dalthon", "senha!", "POST", "/statuses/update.xml");
-		requisicao.addParam("status", "POXA EH INCRIVEL COMO TWITTAR VIA UM PROGRAMINHA ESTUPIDO EM JAVA ENCHE O SACO!");
+		requisicao = new Requisicao("dalthon", "TWITTER3fqfgr", "POST", "/statuses/update.xml");
+		requisicao.addParam("status", "O ALEXANDRE NAO ACREDITA QUE FUNCIONA, NEM EU!");
 		tunel.enviarRequisicao(new Telefone("99998888"), requisicao);
 	}
 
