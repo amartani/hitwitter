1eac16a9b22c3a3eca3b1df2772888f1e92f2bb2
dalthon <dalthon@localhost>
2010-07-12 12:18:45 -0300

    Antes de importar HTTPClient

--- src/entidades/Requisicao.java
+++ src/entidades/Requisicao.java
@@ -4,5 +4,37 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Requisicao implements Serializable {
+	String login;
+	String password;
+	String method;
+	String url;
 
+	public Requisicao(	String login, String password, String method, String url) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.method = method;
+		this.url = url;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+	
+	@Override
+	public String toString() {
+		return getMethod() + " " + getUrl();
+	}
 }
--- src/entidades/RespostaDeRequisicao.java
+++ src/entidades/RespostaDeRequisicao.java
@@ -4,5 +4,26 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class RespostaDeRequisicao implements Serializable {
+	String errorMessage = "";
+	String statusCode = "";
+	String message = "";
 
+	public RespostaDeRequisicao(String errorMessage, String statusCode, String message) {
+		super();
+		this.errorMessage = errorMessage;
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
 }
