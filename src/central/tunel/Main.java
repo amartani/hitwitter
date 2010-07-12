6b8f8755b702bdb5b0a3506ffa884cc5a2e79c52
dalthon <dalthon@localhost>
2010-07-12 15:37:09 -0300

    Backend do Twitter funcionando! falta a interface grafica agora!

--- src/central/tunel/Main.java
+++ src/central/tunel/Main.java
@@ -1,13 +1,21 @@
 package central.tunel;
 
+import java.util.HashMap;
+
+import entidades.Requisicao;
+import entidades.Telefone;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Tunel tunel = new Tunel();
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("status", "POXA EH INCRIVEL COMO TWITTAR VIA UM PROGRAMINHA ESTUPIDO EM JAVA ENCHE O SACO!");
+		Requisicao requisicao = new Requisicao("dalthon", "senha!", "POST", "/statuses/update.xml", params);
+		tunel.enviarRequisicao(new Telefone("99998888"), requisicao);
 	}
 
 }
