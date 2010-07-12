bad00a36a5743ebdae5047cd5843e37abeeb375f
dalthon <dalthon@localhost>
2010-07-12 15:47:00 -0300

    Example of how to tweet and read tweets

--- src/central/tunel/Main.java
+++ src/central/tunel/Main.java
@@ -13,8 +13,11 @@ public class Main {
 	public static void main(String[] args) {
 		Tunel tunel = new Tunel();
 		HashMap<String, String> params = new HashMap<String, String>();
+		// Ler tweets
+		Requisicao requisicao = new Requisicao("dalthon", "senha!", "GET", "/statuses/friends_timeline.xml", params);
+		// Twittar!
 		params.put("status", "POXA EH INCRIVEL COMO TWITTAR VIA UM PROGRAMINHA ESTUPIDO EM JAVA ENCHE O SACO!");
-		Requisicao requisicao = new Requisicao("dalthon", "senha!", "POST", "/statuses/update.xml", params);
+		requisicao = new Requisicao("dalthon", "senha!", "POST", "/statuses/update.xml", params);
 		tunel.enviarRequisicao(new Telefone("99998888"), requisicao);
 	}
 
