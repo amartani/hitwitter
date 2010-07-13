5e4cf78dce174715a9b5aa32a2705960156daf0d
William <William@localhost>
2010-07-13 04:04:49 -0300

    coree????es na tela ler tweets

---
+++ src/central/telefone/ip/IIPChamada.java
@@ -0,0 +1,33 @@
+package central.telefone.ip;
+
+import entidades.Telefone;
+
+public interface IIPChamada {
+	/**
+	 * Armazena uma chamada em andamento
+	 * 
+	 * @param chave
+	 *            Telefone chave (origem ou destino)
+	 * @param interlocutor
+	 *            Telefone interlocutor (destino ou origem)
+	 */
+	public abstract void inserir(Telefone chave, Telefone interlocutor);
+	
+	/**
+	 * Procura um telefone no banco
+	 * 
+	 * @param telefone
+	 *            Telefone a procurar
+	 * @return Telefone interlocutor referente à chamada atual, ou null se não estiver ocupado
+	 */
+	public abstract Telefone procurar(Telefone telefone);
+	
+	/**
+	 * Apaga o telefone do banco
+	 * 
+	 * @param telefone
+	 *            Telefone a apagar
+	 */
+	public abstract void apagar(Telefone telefone);	
+}
+
